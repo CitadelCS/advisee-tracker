8862b110d565b02de539001f5da3949b2f4e5d61
Ryan Skibicki <rskibick@citadel.edu>
2020-10-29 23:00:04 +0000

    the page was made

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,10 +2,19 @@ class StudentsController < ApplicationController
     
     
     
-    def new
-    # default: render 'new' template
+  def new
+    # default: render 'addnew' template
   end
     
-    
+    def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to "/students/index", notice: "Logged in"
+    else
+      flash.now[:alert] = "Invalid entry, please try again"
+      render "new"
+    end
+  end
     
 end
