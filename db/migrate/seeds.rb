0fd0c8e0ccabdf470c49d6f66218c47db9306c36
Skibicks <72174283+Skibicks@users.noreply.github.com>
2020-10-30 15:16:05 -0400

    Update seeds.rb
    changed : 
    
    add_student = [{:first => 'Joe', :last => 'Smith', :CWID => '123452', :DOB => '26-Oct-1984', :advisor => "Dr V.", :graduationYear => '2020', :graduationSemester => 'Spring'},
        	  [{:first => 'Jose', :last => 'Smithy', :CWID => '123453', :DOB => '26-Oct-1985', :advisor => "Dr V.", :graduationYear => '2020', :graduationSemester => 'Spring'}
      	     ] 
    
    add_student.each do |student|

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -1,7 +1,8 @@
-add_student = [{:first => 'Joe', :last => 'Smith', :CWID => '123452', :DOB => '26-Oct-1984', :advisor => "Dr V.", :graduationYear => '2020', :graduationSemester => 'Spring'},
-    	  [{:first => 'Jose', :last => 'Smithy', :CWID => '123453', :DOB => '26-Oct-1985', :advisor => "Dr V.", :graduationYear => '2020', :graduationSemester => 'Spring'}
-  	     ]
+students = [{:first => 'Joe', :last => 'Allan', :CWID => '5336587', :DOB => '15-Feb-2000', :advisor => 'Dr. V', :year => '2021', :semester => 'Spring', :lastMeet => '29-Oct-2022'},
+    	  {:first => 'Ryan', :last => 'Arnold', :CWID => '11183184', :DOB => '4-Aug-2000', :advisor => 'Dr. V', :year => '2021', :semester => 'Spring', :lastMeet => '30-Oct-2022'},
+          {:first => 'Jeff', :last => 'Avery', :CWID => '9564086', :DOB => '18-Apr-2001', :advisor => 'Dr. V', :year => '2021', :semester => 'Spring', :lastMeet => '31-Oct-2022'},
+  	      ]
 
-add_student.each do |student|
+students.each do |student
   Student.create!(student)
 end
