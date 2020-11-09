f79380e697fbab238fff17724d6a07b11291c21f
John Kreisler <jkreisl1@citadel.edu>
2020-11-09 04:20:49 +0000

    added a working add student page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
     
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :students, only: [:new, :create, :index]
+    
     
   root 'home#index'
 
@@ -13,13 +15,13 @@ Rails.application.routes.draw do
     
   get 'students/index'
     
-  get 'students/new'
+  #get 'students/new'
     
     
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-  get 'addnew', to: 'students#new', as: 'addnew'
-  get 'students', to: 'students#index', as: 'students'
+  #get 'addnew', to: 'students#new', as: 'addnew'
+  #get 'students', to: 'students#index', as: 'students'
   
 end
